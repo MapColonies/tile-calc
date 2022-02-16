0a27ffca04b95155f58de587c9b833e727c5b5af
Vitali Ginzburg <54726763+vitaligi@users.noreply.github.com>
2022-02-16 14:38:34 +0200

    test: reflect specific range error

--- tests/unit/tiles.spec.ts
+++ tests/unit/tiles.spec.ts
@@ -91,7 +91,7 @@ describe('#boundingBoxToTiles', () => {
       boundingBoxToTiles(bbox, zoom);
     };
 
-    expect(badTilesGenerator).toThrow(Error("bounding box's west must be larger or equal than the west value of tile grid's bounding box"));
+    expect(badTilesGenerator).toThrow(RangeError("bounding box's west must be larger or equal than the west value of tile grid's bounding box"));
   });
   it("should throw an error when the given bounding box's east value is larger than tile grid's bounding box east value", () => {
     const bbox: BoundingBox = { west: 30, south: -30, east: 190, north: 30 };
@@ -101,7 +101,7 @@ describe('#boundingBoxToTiles', () => {
       boundingBoxToTiles(bbox, zoom);
     };
 
-    expect(badTilesGenerator).toThrow(Error("bounding box's east must be smaller or equal than the east value of tile grid's bounding box"));
+    expect(badTilesGenerator).toThrow(RangeError("bounding box's east must be smaller or equal than the east value of tile grid's bounding box"));
   });
   it("should throw an error when the given bounding box's south value is less than tile grid's bounding box south value", () => {
     const bbox: BoundingBox = { west: 30, south: -100, east: 40, north: 30 };
@@ -111,7 +111,7 @@ describe('#boundingBoxToTiles', () => {
       boundingBoxToTiles(bbox, zoom);
     };
 
-    expect(badTilesGenerator).toThrow(Error("bounding box's south must be larger or equal than the south value of tile grid's bounding box"));
+    expect(badTilesGenerator).toThrow(RangeError("bounding box's south must be larger or equal than the south value of tile grid's bounding box"));
   });
   it("should throw an error when the given bounding box's north value is larger than tile grid's bounding box north value", () => {
     const bbox: BoundingBox = { west: 30, south: -30, east: 40, north: 100 };
@@ -121,7 +121,7 @@ describe('#boundingBoxToTiles', () => {
       boundingBoxToTiles(bbox, zoom);
     };
 
-    expect(badTilesGenerator).toThrow(Error("bounding box's north must be smaller or equal than the north value of tile grid's bounding box"));
+    expect(badTilesGenerator).toThrow(RangeError("bounding box's north must be smaller or equal than the north value of tile grid's bounding box"));
   });
 });
 

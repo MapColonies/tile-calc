e2b4608cd95aa5df0e944d3dcd75acd38e8b2fa7
vitaligi <54726763+vitaligi@users.noreply.github.com>
2022-03-06 14:03:12 +0200

    feat: optional clamp to tile grid bbox (#4)
    * feat: implement optional clamping
    
    * test: cover clamp tests & add tile grid tests
    
    * refactor: pr requested changes

--- src/tiles.ts
+++ src/tiles.ts
@@ -3,7 +3,7 @@ import { BoundingBox, LonLat, Tile, TileGrid } from './interfaces';
 import { Limits, Zoom } from './types';
 import { validateLonlat, validateMetatile, validateTile, validateTileGrid, validateTileGridBoundingBox, validateZoomLevel } from './validations';
 
-function clamp(value: number, minValue: number, maxValue: number): number {
+function clampValues(value: number, minValue: number, maxValue: number): number {
   if (value < minValue) {
     return minValue;
   }
@@ -15,13 +15,13 @@ function clamp(value: number, minValue: number, maxValue: number): number {
   return value;
 }
 
-function tileHeight(zoom: Zoom, referenceTileGrid: TileGrid): number {
+function tileProjectedHeight(zoom: Zoom, referenceTileGrid: TileGrid): number {
   return (
     (referenceTileGrid.boundingBox.north - referenceTileGrid.boundingBox.south) / (referenceTileGrid.numberOfMinLevelTilesY * SCALE_FACTOR ** zoom)
   );
 }
 
-function tileWidth(zoom: Zoom, referenceTileGrid: TileGrid): number {
+function tileProjectedWidth(zoom: Zoom, referenceTileGrid: TileGrid): number {
   return (
     (referenceTileGrid.boundingBox.east - referenceTileGrid.boundingBox.west) / (referenceTileGrid.numberOfMinLevelTilesX * SCALE_FACTOR ** zoom)
   );
@@ -38,8 +38,8 @@ function* tilesGenerator(limits: Limits, zoom: Zoom, metatile: number): Generato
 }
 
 function geoCoordsToTile(lonlat: LonLat, zoom: Zoom, metatile = 1, referenceTileGrid: TileGrid = TILEGRID_WORLD_CRS84): Tile {
-  const width = tileWidth(zoom, referenceTileGrid) * metatile;
-  const height = tileHeight(zoom, referenceTileGrid) * metatile;
+  const width = tileProjectedWidth(zoom, referenceTileGrid) * metatile;
+  const height = tileProjectedHeight(zoom, referenceTileGrid) * metatile;
 
   const x = Math.floor((lonlat.lon - referenceTileGrid.boundingBox.west) / width);
   const y = Math.floor((referenceTileGrid.boundingBox.north - lonlat.lat) / height);
@@ -47,8 +47,8 @@ function geoCoordsToTile(lonlat: LonLat, zoom: Zoom, metatile = 1, referenceTile
   // clamp the values in cases when lon is 180 which is calculated as beyond
   // the range of tile index for a given zoom level
   return {
-    x: clamp(x, 0, Math.ceil((referenceTileGrid.numberOfMinLevelTilesX / metatile) * SCALE_FACTOR ** zoom - 1)),
-    y: clamp(y, 0, Math.ceil((referenceTileGrid.numberOfMinLevelTilesY / metatile) * SCALE_FACTOR ** zoom - 1)),
+    x: clampValues(x, 0, Math.ceil((referenceTileGrid.numberOfMinLevelTilesX / metatile) * SCALE_FACTOR ** zoom - 1)),
+    y: clampValues(y, 0, Math.ceil((referenceTileGrid.numberOfMinLevelTilesY / metatile) * SCALE_FACTOR ** zoom - 1)),
     z: zoom,
     metatile: metatile,
   };
@@ -106,26 +106,31 @@ export function lonLatZoomToTile(lonlat: LonLat, zoom: Zoom, metatile = 1, refer
   return geoCoordsToTile(lonlat, zoom, metatile, referenceTileGrid);
 }
 
-export function tileToBoundingBox(tile: Tile, referenceTileGrid: TileGrid = TILEGRID_WORLD_CRS84): BoundingBox {
+export function tileToBoundingBox(tile: Tile, referenceTileGrid: TileGrid = TILEGRID_WORLD_CRS84, clamp = false): BoundingBox {
   validateTileGrid(referenceTileGrid);
   validateTile(tile, referenceTileGrid);
   const metatile = tile.metatile ?? 1;
 
-  const width = tileWidth(tile.z, referenceTileGrid) * metatile;
-  const height = tileHeight(tile.z, referenceTileGrid) * metatile;
+  const width = tileProjectedWidth(tile.z, referenceTileGrid) * metatile;
+  const height = tileProjectedHeight(tile.z, referenceTileGrid) * metatile;
 
-  // clamp the values in cases where a metatile may extend tile bounding box beyond the bounding box
-  // of the tile grid
-  const bbox: BoundingBox = {
-    west: clamp(referenceTileGrid.boundingBox.west + tile.x * width, referenceTileGrid.boundingBox.west, referenceTileGrid.boundingBox.east),
-    south: clamp(
-      referenceTileGrid.boundingBox.north - (tile.y + 1) * height,
-      referenceTileGrid.boundingBox.south,
-      referenceTileGrid.boundingBox.north
-    ),
-    east: clamp(referenceTileGrid.boundingBox.west + (tile.x + 1) * width, referenceTileGrid.boundingBox.west, referenceTileGrid.boundingBox.east),
-    north: clamp(referenceTileGrid.boundingBox.north - tile.y * height, referenceTileGrid.boundingBox.south, referenceTileGrid.boundingBox.north),
+  let bbox: BoundingBox = {
+    west: referenceTileGrid.boundingBox.west + tile.x * width,
+    south: referenceTileGrid.boundingBox.north - (tile.y + 1) * height,
+    east: referenceTileGrid.boundingBox.west + (tile.x + 1) * width,
+    north: referenceTileGrid.boundingBox.north - tile.y * height,
   };
+
+  if (clamp) {
+    // clamp the values in cases where a metatile may extend tile bounding box beyond the bounding box
+    // of the tile grid
+    bbox = {
+      west: clampValues(bbox.west, referenceTileGrid.boundingBox.west, referenceTileGrid.boundingBox.east),
+      south: clampValues(bbox.south, referenceTileGrid.boundingBox.south, referenceTileGrid.boundingBox.north),
+      east: clampValues(bbox.east, referenceTileGrid.boundingBox.west, referenceTileGrid.boundingBox.east),
+      north: clampValues(bbox.north, referenceTileGrid.boundingBox.south, referenceTileGrid.boundingBox.north),
+    };
+  }
 
   return bbox;
 }
